c21ddcbd9eedf2f2490325675b5b45db3fff101e
Francis Chong <francis@ignition.hk>
2015-02-28 11:57:37 +0800

    add syllable

--- lib/cantonese/syllables/scraper.rb
+++ lib/cantonese/syllables/scraper.rb
@@ -6,11 +6,35 @@ module Cantonese
   module Syllables
     class Scraper
       LIST_URL = "http://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/syllables.php"
+      FINAL_REGEXP = %r{phonetic_final\[[0-9]+\] = "([^"]+)";}
+      TONE_REGEXP = %r{phonetic_tone\[[0-9]+\] = "([^"]+)";}
 
       # get syllables list
       def self.list
+        base_url = URI.parse("http://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/syllables.php")
         doc = Nokogiri::HTML(HTTP.get(LIST_URL).body.to_s)
-        doc.search("#syllables_table td a").collect { |node| node.text }
+        doc.search("#syllables_table td a").collect do |node| 
+          text = node.text
+          url = base_url.merge(node['href']).to_s
+          {text: text, url: url }
+        end
+      end
+
+      # get detail for a syllable
+      def self.syllable(url)
+        doc = Nokogiri::HTML(HTTP.get(url).body.to_s)
+        doc.search(".pho-rel tr").collect do |node|
+          cells = node.search("td")
+          js = cells[0].text rescue nil
+          phonetic = js.match(FINAL_REGEXP)[1] + js.match(TONE_REGEXP)[1] rescue nil
+          text = cells[2].text.split(",").collect{|text| text.strip } rescue nil
+
+          if text && phonetic
+            {syllable: phonetic, characters: text}
+          else
+            nil
+          end
+        end.compact
       end
     end
   end
--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -4,9 +4,18 @@ describe Cantonese::Syllables::Scraper do
   let (:'scraper') { Cantonese::Syllables::Scraper }
   context "#list" do
     it "should return syllable list" do
-      expect(scraper.list).to be_a(Array)
-      expect(scraper.list).to be_include("啊")
-      expect(scraper.list).to be_include("哼")
+      list = scraper.list
+      expect(list).to be_a(Array)
+      expect(list).to be_include(text: "啊", url: "http://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/pho-rel.php?initial=-&final=aa")
+      expect(list).to be_include(text: "哼", url: "http://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/pho-rel.php?initial=h&final=ng")
+    end
+  end
+
+  context "#syllable" do
+    it "should return syllable details" do
+      syllable = scraper.syllable("http://humanum.arts.cuhk.edu.hk/Lexis/lexi-mf/pho-rel.php?initial=-&final=aa")
+      expect(syllable).to be_a(Array)
+      expect(syllable).to be_include(syllable: "aa4", characters: ["啊"])
     end
   end
 end
